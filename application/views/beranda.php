2f001c4911fc8573f63e9b624aaffbcfa6897618
agusrahmat18 <agus.rahmat.18@gmail.com>
2023-02-28 16:29:06 +0700

    update 28 februari 2023

--- application/views/beranda.php
+++ application/views/beranda.php
@@ -1,3 +1,12 @@
+<style>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+</style>
+
 <body class="page-index">
 
   
@@ -7,14 +16,14 @@
     <div class="container">
       <div class="row">
         <div class="col-xl-6">
-          <h2 data-aos="fade-up" syle="color:white;">PUSAT KRISIS DAN KEGAWATDARURATAN KESEHATAN DAERAH</h2>
+          <h2 data-aos="fade-up" style="color:white; text-align:left; font-size:40px;">PUSAT KRISIS DAN KEGAWATDARURATAN KESEHATAN DAERAH</h2>
           <div class="col-xl-8">
             <blockquote data-aos="fade-up" data-aos-delay="100">
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis cum recusandae eum laboriosam voluptatem repudiandae odio, vel exercitationem officiis provident minima. </p>
             </blockquote>
             <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
               <a href="#about" class="btn-get-started">Get Started</a>
-              <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+              <a href="https://youtu.be/6cr6rtqixDU" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
             </div>
           </div>
         </div>
@@ -29,49 +38,64 @@
       <div class="container" data-aos="fade-up">
 
         <div class="section-header">
-          <h2>Why Choose Us</h2>
+          <h2>Pelayanan 24 Jam</h2>
 
         </div>
 
         <div class="row g-0" data-aos="fade-up" data-aos-delay="200">
 
-          <div class="col-xl-5 img-bg" style="background-image: url('<?php echo base_url(); ?>assets/img/why-us-bg.jpg')"></div>
-          <div class="col-xl-7 slides  position-relative">
+        <!-- <div class="col-xl-4 img-bg" style="width:30%; background-image: url('<?php echo base_url(); ?>assets/img/banner.jpg');"></div> -->
+          <div class="col-xl-5 img-bg">
+            <img src="<?php echo base_url(); ?>assets/img/banner.jpg" style="width:100%;">
+          </div>
 
+          <div class="col-xl-7 slides position-relative">
             <div class="slides-1 swiper">
               <div class="swiper-wrapper">
-
                 <div class="swiper-slide">
                   <div class="item">
-                    <h3 class="mb-3">Let's grow your business together</h3>
+                    <img class="mb-3 center" src="<?php echo base_url(); ?>assets/img/hero-bg.jpg" style="width:100%;">
+                    <h3 class="mb-3">Ambulans Advance</h3>
                     <h4 class="mb-3">Optio reiciendis accusantium iusto architecto at quia minima maiores quidem, dolorum.</h4>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, ipsam perferendis asperiores explicabo vel tempore velit totam, natus nesciunt accusantium dicta quod quibusdam ipsum maiores nobis non, eum. Ullam reiciendis dignissimos laborum aut, magni voluptatem velit doloribus quas sapiente optio.</p>
                   </div>
-                </div><!-- End slide item -->
+                </div>
 
                 <div class="swiper-slide">
                   <div class="item">
-                    <h3 class="mb-3">Unde perspiciatis ut repellat dolorem</h3>
+                    <img class="mb-3 center" src="<?php echo base_url(); ?>assets/img/hero-bg.jpg" style="width:100%">
+                    <h3 class="mb-3">Ambulans Infeksi</h3>
                     <h4 class="mb-3">Amet cumque nam sed voluptas doloribus iusto. Dolorem eos aliquam quis.</h4>
                     <p>Dolorem quia fuga consectetur voluptatem. Earum consequatur nulla maxime necessitatibus cum accusamus. Voluptatem dolorem ut numquam dolorum delectus autem veritatis facilis. Et ea ut repellat ea. Facere est dolores fugiat dolor.</p>
                   </div>
-                </div><!-- End slide item -->
+                </div>
 
                 <div class="swiper-slide">
                   <div class="item">
-                    <h3 class="mb-3">Aliquid non alias minus</h3>
+                    <img class="mb-3 center" src="<?php echo base_url(); ?>assets/img/hero-bg.jpg" style="width:100%">
+                    <h3 class="mb-3">Ambulans Nets</h3>
                     <h4 class="mb-3">Necessitatibus voluptatibus explicabo dolores a vitae voluptatum.</h4>
                     <p>Neque voluptates aut. Soluta aut perspiciatis porro deserunt. Voluptate ut itaque velit. Aut consectetur voluptatem aspernatur sequi sit laborum. Voluptas enim dolorum fugiat aut.</p>
                   </div>
-                </div><!-- End slide item -->
+                </div>
 
                 <div class="swiper-slide">
                   <div class="item">
-                    <h3 class="mb-3">Necessitatibus suscipit non voluptatem quibusdam</h3>
+                    <img class="mb-3 center" src="<?php echo base_url(); ?>assets/img/hero-bg.jpg" style="width:100%">
+                    <h3 class="mb-3">Unit Reaksi Cepat</h3>
                     <h4 class="mb-3">Tempora quos est ut quia adipisci ut voluptas. Deleniti laborum soluta nihil est. Eum similique neque autem ut.</h4>
                     <p>Ut rerum et autem vel. Et rerum molestiae aut sit vel incidunt sit at voluptatem. Saepe dolorem et sed voluptate impedit. Ad et qui sint at qui animi animi rerum.</p>
                   </div>
-                </div><!-- End slide item -->
+                </div>
+
+                <div class="swiper-slide">
+                  <div class="item">
+                    <img class="mb-3 center" src="<?php echo base_url(); ?>assets/img/hero-bg.jpg" style="width:100%">
+                    <h3 class="mb-3">Kapal Ambulans Kepulauan Seribu</h3>
+                    <h4 class="mb-3">Tempora quos est ut quia adipisci ut voluptas. Deleniti laborum soluta nihil est. Eum similique neque autem ut.</h4>
+                    <p>Ut rerum et autem vel. Et rerum molestiae aut sit vel incidunt sit at voluptatem. Saepe dolorem et sed voluptate impedit. Ad et qui sint at qui animi animi rerum.</p>
+                  </div>
+                </div>
 
               </div>
               <div class="swiper-pagination"></div>
@@ -87,23 +111,19 @@
 
     <!-- ======= Our Services Section ======= -->
     <section id="services-list" class="services-list">
-      <div class="container" data-aos="fade-up">
-
+      <!-- <div class="container" data-aos="fade-up">
         <div class="section-header">
           <h2>Our Services</h2>
-
         </div>
 
         <div class="row gy-5">
-
           <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="100">
             <div class="icon flex-shrink-0"><i class="bi bi-briefcase" style="color: #f57813;"></i></div>
             <div>
               <h4 class="title"><a href="#" class="stretched-link">Lorem Ipsum</a></h4>
               <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
             </div>
-          </div>
-          <!-- End Service Item -->
+          </div>          
 
           <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="200">
             <div class="icon flex-shrink-0"><i class="bi bi-card-checklist" style="color: #15a04a;"></i></div>
@@ -111,7 +131,7 @@
               <h4 class="title"><a href="#" class="stretched-link">Dolor Sitema</a></h4>
               <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="300">
             <div class="icon flex-shrink-0"><i class="bi bi-bar-chart" style="color: #d90769;"></i></div>
@@ -119,7 +139,7 @@
               <h4 class="title"><a href="#" class="stretched-link">Sed ut perspiciatis</a></h4>
               <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="400">
             <div class="icon flex-shrink-0"><i class="bi bi-binoculars" style="color: #15bfbc;"></i></div>
@@ -127,7 +147,7 @@
               <h4 class="title"><a href="#" class="stretched-link">Magni Dolores</a></h4>
               <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="500">
             <div class="icon flex-shrink-0"><i class="bi bi-brightness-high" style="color: #f5cf13;"></i></div>
@@ -135,7 +155,7 @@
               <h4 class="title"><a href="#" class="stretched-link">Nemo Enim</a></h4>
               <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
             </div>
-          </div><!-- End Service Item -->
+          </div>
 
           <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="600">
             <div class="icon flex-shrink-0"><i class="bi bi-calendar4-week" style="color: #1335f5;"></i></div>
@@ -143,11 +163,9 @@
               <h4 class="title"><a href="#" class="stretched-link">Eiusmod Tempor</a></h4>
               <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
             </div>
-          </div><!-- End Service Item -->
-
+          </div>
         </div>
-
-      </div>
+      </div> -->
     </section><!-- End Our Services Section -->
 
     <!-- ======= Call To Action Section ======= -->
@@ -233,11 +251,47 @@
           </div>
           <div class="col-lg-5 position-relative" data-aos="fade-up" data-aos-delay="200">
             <div class="phone-wrap">
-              <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid">
+              <!-- <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid"> -->
+              <!-- <div class="slides-1 swiper"> -->
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <div class="item">
+                      <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid">
+                    </div>
+                  </div>
+                  <div class="swiper-slide">
+                    <div class="item">
+                      <img src="<?php echo base_url(); ?>assets/img/banner.jpg" alt="Image" class="img-fluid">
+                    </div>
+                  </div>
+                </div>
+              <!-- </div> -->
             </div>
+
+            <!-- <div class="slides-1 swiper">
+              <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                  <div class="item">
+                    <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid">
+                  </div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="item">
+                    <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid"></div>
+                </div>
+                <div class="swiper-slide">
+                  <div class="item">
+                    <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid"></div>
+                </div>                
+                <div class="swiper-slide">
+                  <div class="item">
+                    <img src="<?php echo base_url(); ?>assets/img/iphone.png" alt="Image" class="img-fluid"></div>
+                </div>
+              </div>
+            </div> -->
+
           </div>
         </div>
-
       </div>
 
       <div class="details">
@@ -324,4 +378,3 @@
     </section><!-- End Recent Blog Posts Section -->
 
   </main><!-- End #main -->
-
